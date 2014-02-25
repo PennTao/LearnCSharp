1d0a8e283559eb08dd3fa4dfc34a26b281092482
PennTao <whu.eis.lt@gmail.com>
2014-02-24 23:48:53 -0500

    add check binary tree balance

---
+++ CheckTreeBalance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckTreeBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(4);
+            Console.WriteLine("add root");
+            root.left = new TreeNode(5);
+            Console.WriteLine("add root left");
+          
+
+            if (isBalance(root) == true)
+            {
+                Console.WriteLine("balance!");
+            }
+            else
+            {
+                Console.WriteLine("not balance!");
+            }
+            root.right = new TreeNode(6);
+            Console.WriteLine("add root right");
+            root.right.right = new TreeNode(7);
+            Console.WriteLine("add root right right");
+            root.right.right.right = new TreeNode(8);
+            Console.WriteLine("add root right right right");
+            if (isBalance(root) == true)
+            {
+                Console.WriteLine("balance!");
+            }
+            else
+            {
+                Console.WriteLine("not balance!");
+            }
+        }
+        static bool isBalance(TreeNode root)
+        {
+            if (root == null)
+                return true;
+            else
+                return Math.Abs(depth(root.left) - depth(root.right)) < 2;
+        }
+        static int depth(TreeNode root)
+        {
+            
+            if (root == null)
+                return 0;
+            
+            int ldepth = depth(root.left);
+            int rdepth = depth(root.right);
+            
+            return Math.Max(ldepth, rdepth) + 1;
+        }
+    }
+}
---
+++ CheckTreeBalance/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckTreeBalance
+{
+    class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+        private int val;
+        public TreeNode(int a)
+        {
+            val = a;
+        }
+
+    }
+}
