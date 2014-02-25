498263e7687cc350918cab9f6f9e4f37703f4c34
PennTao <whu.eis.lt@gmail.com>
2014-02-24 21:41:12 -0500

    Queue implemented with two stacks

---
+++ MyQueue/MyQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    class MyQueue<T>
+    {
+        private Stack<T> stk1;
+        private Stack<T> stk2;
+
+        public MyQueue()
+        {
+            stk1 = new Stack<T>();
+            stk2 = new Stack<T>();
+        }
+
+        public void Enqueue(T val)
+        {
+            stk1.Push(val);
+        }
+        public T Dequeue()
+        {
+            T result;
+            while (stk1.Count > 0)
+            {
+                stk2.Push(stk1.Pop());
+            }
+            result = stk2.Pop();
+            while (stk2.Count > 0)
+            {
+                stk1.Push(stk2.Pop());
+            }
+                return result;
+        }
+        public int size()
+        {
+            return stk1.Count;
+        }
+    }
+}
---
+++ MyQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyQueue<int> queue = new MyQueue<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+            while (queue.size() > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
