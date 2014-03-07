53af4ba3ad8ed0ee2237dfbc4c3a0d55bad482d8
PennTao <whu.eis.lt@gmail.com>
2014-03-07 17:47:57 -0500

    fill paint

---
+++ PaintFill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Implement Fill funtion in Paint*/
+namespace PaintFill
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+        static void Fill(ref int[,] canvas, int oColor, int dColor, int x, int y)
+        {
+            if (x < 0 || x > canvas.GetLength(0) ||y < 0 || y > canvas.GetLength(1))
+            {
+                return;
+            }
+            if(canvas[x,y] == oColor) 
+            {
+                canvas[x, y] = dColor;
+                Fill(ref canvas, oColor, dColor, x - 1, y);
+                Fill(ref canvas, oColor, dColor, x+1 , y);
+                Fill(ref canvas, oColor, dColor, x , y - 1);
+                Fill(ref canvas, oColor, dColor, x, y + 1);
+            }
+        }
+    }
+}
